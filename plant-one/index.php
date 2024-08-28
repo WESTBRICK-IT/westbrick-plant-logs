ed1d326d5f3708c8aa7d98365660d92336860028
Chris Barber <cbarber@westbrick.ca>
2024-08-28 12:57:39 -0600

    finished index.php

--- plant-one/index.php
+++ plant-one/index.php
@@ -37,19 +37,147 @@
             die("Connection failed: " . mysqli_connect_error());
         }
 
-        $query = "SELECT * FROM `links` ORDER BY `first_id` DESC, `second_id` DESC";
+        $query = "SELECT * FROM `westbrick_plant_log1` ORDER BY `id` DESC, `author` DESC";
         $result = mysqli_query($conn, $query);
         if (mysqli_num_rows($result) > 0) {                                            
             while($row = mysqli_fetch_assoc($result)) {
+                                
+                $id = $row['id'];                
+                $author = $row['author'];
+                $shift = $row['shift'];
+                $operator1 = $row['operator1'];
+                $operator2 = $row['operator2'];
+                $operator3 = $row['operator3'];
+                $shiftHandoverMeeting = $row['shift_handover_meeting'];
+                $startOfShiftMeeting = $row['start_of_shift_meeting'];
+                $plantStatus = $row['plant_status'];
+                $equipmentOutage = $row['equipment_outage'];
+                $filterChange = $row['filter_change'];
+                $pigging = $row['pigging'];
+                $recyclePumps = $row['recycle_pumps'];
+                $productionTankLevel = $row['production_tank_level'];
+                $lPG_BulletPeakLevel = $row['lpg_bullet_peak_level'];
+                $lPG_BulletPeakPressure = $row['lpg_bullet_peak_pressure'];
+                $bermWaterSamplesTaken = $row['berm_water_samples_taken'];
+                $plantProcessDiscussion = $row['plant_process_discussion'];
+                $operationalTargets = $row['operational_targets'];
+                $overRidesOrSafetiesBypassed = $row['overrides'];
+                $upcomingActivities = $row['upcoming_activities'];
+                $hSE_Concerns = $row['hse_concerns'];
+                $regulatoryRequirements = $row['regulatory_requirements'];
+                $staffDiscussion = $row['staff_discussion'];
+                $weatherAndEffectsOnOperations = $row['weather_and_effects_on_operations'];
+                $permitExtensionsCriticalTasks = $row['permit_extensions_critical_tasks'];
+                $remark = $row['remark'];
+                $date = $row['date'];
+                $time = $row['time'];
+
                 
-                $id = $row['id'];
-                
-                echo    "   <h1 class='log-title'>Log #$id</h1>";
-                
-                
-                echo    "           <td><img class='garbage-can garbage-can$id links-garbage-can links-garbage-can$id' src='../images/garbage-can.svg' alt='links Garbage Can $id'></td>";
-                echo    "       </tr>"; 
-                
+                echo    "   <h1 class='log-title'>Plant Log #$id</h1>";
+                echo    "   <div class='table-wrapper'>";
+                echo    "       <table class='sub-menu-table'>";
+                echo    "           <thead>";
+                echo    "               <tr>";
+                echo    "                   <th>Date</th>";
+                echo    "                   <th>Time</th>";                
+                echo    "                   <th>Message ID</th>";
+                echo    "                   <th>Author</th>";
+                echo    "                   <th>Shift</th>";
+                echo    "                   <th>Operator 1</th>";
+                echo    "                   <th>Operator 2</th>";
+                echo    "                   <th>Operator 3</th>";
+                echo    "                   <th>Shift Handover Meeting</th>";
+                echo    "               </tr>";
+                echo    "           </thead>";
+                echo    "           <tbody>";
+                echo    "               <tr>";
+                echo    "                   <td>$date</td>";
+                echo    "                   <td>$time</td>";
+                echo    "                   <td>$id</td>";
+                echo    "                   <td>$author</td>";
+                echo    "                   <td>$shift</td>";
+                echo    "                   <td>$operator1</td>";
+                echo    "                   <td>$operator2</td>";
+                echo    "                   <td>$operator3</td>";
+                echo    "                   <td>$shiftHandoverMeeting</td>";
+                echo    "               </tr>";
+                echo    "           </tbody>";
+                echo    "       </table>";
+                echo    "       <table class='sub-menu-table'>";
+                echo    "           <thead>";
+                echo    "               <tr>";
+                echo    "                   <th>Start of Shift Meeting</th>";
+                echo    "                   <th>Plant Status</th>";
+                echo    "                   <th>Equipment Outage</th>";
+                echo    "                   <th>Filter Change</th>";
+                echo    "                   <th>Pigging</th>";
+                echo    "                   <th>Recycle Pumps</th>";
+                echo    "                   <th>Production Tank Level</th>";
+                echo    "               <tr>";
+                echo    "           </thead>";
+                echo    "           <tbody>";
+                echo    "               <tr>";
+                echo    "                   <td>$startOfShiftMeeting</td>";
+                echo    "                   <td>$plantStatus</td>";
+                echo    "                   <td>$equipmentOutage</td>";
+                echo    "                   <td>$filterChange</td>";
+                echo    "                   <td>$pigging</td>";
+                echo    "                   <td>$recyclePumps</td>";
+                echo    "                   <td>$productionTankLevel</td>";
+                echo    "               </tr>";
+                echo    "           </tbody>";
+                echo    "       </table>";
+                echo    "       <table class='sub-menu-table'>";
+                echo    "           <thead>";
+                echo    "               <tr>";
+                echo    "                   <th>LPG Bullet Peak Level</th>";
+                echo    "                   <th>LPG Bullet Peak Pressure</th>";
+                echo    "                   <th>Berm Water Samples Taken</th>";
+                echo    "                   <th>Plant Process Discussion</th>";
+                echo    "                   <th>Operational Targets</th>";
+                echo    "                   <th>Recycle Pumps</th>";                
+                echo    "                   <th>Production Tank Level</th>";
+                echo    "               </tr>";
+                echo    "           </thead>";
+                echo    "           <tbody>";
+                echo    "               <tr>";
+                echo    "                   <th>$lPG_BulletPeakLevel</th>";
+                echo    "                   <th>$lPG_BulletPeakPressure</th>";
+                echo    "                   <th>$bermWaterSamplesTaken</th>";
+                echo    "                   <th>$plantProcessDiscussion</th>";
+                echo    "                   <th>$operationalTargets</th>";
+                echo    "                   <th>$recyclePumps</th>";
+                echo    "                   <th>$productionTankLevel</th>";
+                echo    "               </tr>";
+                echo    "           </tbody>";
+                echo    "       </table>";
+                echo    "       <table class='sub-menu-table'>";
+                echo    "           <thead>";
+                echo    "               <tr>";
+                echo    "                   <th>Upcoming Activities</th>";
+                echo    "                   <th>HSE Concerns</th>";
+                echo    "                   <th>Regulatory Requirements</th>";
+                echo    "                   <th>Staff Discussion</th>";
+                echo    "                   <th>Weather & Effects On Operations</th>";
+                echo    "                   <th>Permit Extensions/Critical Tasks</th>";                
+                echo    "               </tr>";
+                echo    "           </thead>";
+                echo    "           <tbody>";
+                echo    "               <tr>";
+                echo    "                   <th>$upcomingActivities</th>";
+                echo    "                   <th>$hSE_Concerns</th>";
+                echo    "                   <th>$regulatoryRequirements</th>";
+                echo    "                   <th>$staffDiscussion</th>";
+                echo    "                   <th>$weatherAndEffectsOnOperations</th>";
+                echo    "                   <th>$permitExtensionsCriticalTasks</th>";
+                echo    "               </tr>";
+                echo    "           </tbody>";
+                echo    "       </table>";
+                echo    "       <p class='plant-log-remarks'>";
+                echo    "           $remark";
+                echo    "       </p>";
+                echo    "       <td><img class='garbage-can garbage-can$id links-garbage-can links-garbage-can$id' src='../images/garbage-can.svg' alt='links Garbage Can $id'></td>";                
+                echo    "   </div>";                
             }
         }
 
